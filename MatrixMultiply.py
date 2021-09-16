f3a6a6dd7a6f9ca1b14c6e180673cd12364d9094
Ryan Rivero <rmrivero@miners.utep.edu>
2021-09-16 01:11:45 -0600

    Begun to parallize matrix multiply function. Seeing slightly better time results however nothing too drastic. Results change when threads are added. Regular matrix multiply sometimes shows a slight better result in time

--- MatrixMultiply.py
+++ MatrixMultiply.py
@@ -1,18 +1,31 @@
 import matrixUtils
 import numpy as np
 import time
+import pymp
 
 def matrixMultiply(a, b):
-
-    tp = np.zeros((a.shape[0], b.shape[1]), dtype = np.int16)
-    c = tp.reshape(a.shape[0], b.shape[1])
+    
+    c = np.zeros((a.shape[0], b.shape[1]), dtype = np.int16)
 
     for i in range(len(a)):
         for j in range(len(b[0])):
-            for k in range(len(b)):r
+            for k in range(len(b)):
                 c[i][j] += a[i][k] * b[k][j]
     return c
 
+def matrixMultiplyPar(a, b):
+
+    c = np.zeros((a.shape[0], b.shape[1]), dtype = np.int16)
+    
+    with pymp.Parallel() as p:
+
+        for i in range(len(a)):
+            for j in range(len(b[0])):
+                for k in p.range(len(b)):
+                    c[i][j] += a[i][k] * b[k][j]
+        
+    return c
+"""
 def blocked(a, b):
     tile_size = 16
 
@@ -27,23 +40,24 @@ def blocked(a, b):
                         sum = sum + in_a[i][k] * in_b[k][j]
                     output[i][j] =  sum
     return output
-
+"""
 def main():
 
-    tmp1 = np.arange(1, 17)
-    tmp2 = np.arange(16, 32)
-    a = tmp1.reshape(4, 4)
-    b = tmp2.reshape(4, 4)
-
-    print(a)
-    print(b)
+    a = matrixUtils.genMatrix2(32, 2);
+    b = matrixUtils.genMatrix2(32, 2)
 
     start = time.clock_gettime(time.CLOCK_MONOTONIC)
-    c = matrixMultiply(a, b)
+    MMP = matrixMultiply(a, b)
     end = time.clock_gettime(time.CLOCK_MONOTONIC)
+    
+    startPar = time.clock_gettime(time.CLOCK_MONOTONIC)
+    MM = matrixMultiplyPar(a, b)
+    endPar = time.clock_gettime(time.CLOCK_MONOTONIC)
+    
     print('New Matrix:')
-    print(c)
-    print("Time to process: ", str(end - start))
+    print(MM)
+    print("Time to process using regular Matrix Multiply: ", str(end - start))
+    print("Time to process using Parallized Matrix Multiply: ", str(endPar - startPar))
 
 if __name__ =='__main__':
     main()
